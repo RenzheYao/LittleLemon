ec852898c46834c366b84af196b308992516c2ae
renzhe yao <renzheyao@gmail.com>
2025-10-23 02:22:33 +0200

    Authentication

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,12 @@
+from rest_framework.decorators import permission_classes, api_view
 from django.shortcuts import render
-from rest_framework import generics, viewsets, permissions
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.response import Response
+
+
 
 # Create your views here.
 def index(request):
@@ -10,6 +15,7 @@ def index(request):
 class MenuItemView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
@@ -19,4 +25,13 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
+    
+    
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def test_token(request):
+    return Response({
+        "message": "Token works!",
+        "user": request.user.username
+    })
